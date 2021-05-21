70244055ab2c4ab5baffad36847951080effe9bd
oracle <txmlconnector@primacomp.ru>
2021-05-22 00:54:21 +0300

    PREPARATION FOR LOADING *.BPMN AT STARTUP

--- src/main/groovy/ru/quickel/Main.groovy
+++ src/main/groovy/ru/quickel/Main.groovy
@@ -1,11 +1,28 @@
 package ru.quickel
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+import java.nio.file.Path;
 
 @SpringBootApplication
 public class Main {
     public static void main(String... args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(Main.class, args)
+        print("Spring is loaded\n")
+        loadBpmn()
+    }
+
+    static Closure loadBpmn = {
+        File currentDir = new File("bpmn\\").getAbsoluteFile()
+        assert currentDir.isDirectory() == true
+        currentDir.listFiles().findAll {
+            File file ->
+                file.name.containsIgnoreCase(".bpmn")
+        }.each {
+            File currentFile ->
+                print("${currentFile}\n")
+        }
+        //todo: Необходимо продолжить загрузку *.bpmn файлов при старте
     }
 }
---
+++ src/test/groovy/ru/quickel/TestMain.groovy
@@ -0,0 +1,13 @@
+package ru.quickel
+
+import org.junit.jupiter.api.Test
+
+
+class TestMain {
+
+    @Test
+    void testLoadBpmn(){
+        Main.loadBpmn()
+    }
+
+}
