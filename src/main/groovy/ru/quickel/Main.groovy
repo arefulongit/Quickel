115bea013a1f51cddfceb1ff84af63278844fd9c
oracle <txmlconnector@primacomp.ru>
2021-05-25 23:51:19 +0300

    POM.XML REFACTORY
    Modifying pom.xml to fix rest api error

--- src/main/groovy/ru/quickel/Main.groovy
+++ src/main/groovy/ru/quickel/Main.groovy
@@ -2,27 +2,41 @@ package ru.quickel
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication
-
-import java.nio.file.Path;
+import org.springframework.context.ApplicationContext
+import org.springframework.context.support.ClassPathXmlApplicationContext
+import org.springframework.core.io.FileSystemResourceLoader
+import org.springframework.core.io.Resource
+import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
 public class Main {
     public static void main(String... args) {
-        SpringApplication.run(Main.class, args)
-        print("Spring is loaded\n")
-        loadBpmn()
+        loadSpring(args)
+        //loadResources()
     }
 
-    static Closure loadBpmn = {
+    static void loadSpring(String... args){
+        SpringApplication.run(Main.class, args)
+    }
+
+    static void loadResources() {
         File currentDir = new File("bpmn\\").getAbsoluteFile()
         assert currentDir.isDirectory() == true
         currentDir.listFiles().findAll {
             File file ->
-                file.name.containsIgnoreCase(".bpmn")
+                file.name.containsIgnoreCase(".bpmn") ||
+                        file.name.containsIgnoreCase(".cmn")
         }.each {
-            File currentFile ->
-                print("${currentFile}\n")
+            File bpmnOrCmnFile ->
+                loadResource(bpmnOrCmnFile)
+                print("${bpmnOrCmnFile}\n")
         }
         //todo: Необходимо продолжить загрузку *.bpmn файлов при старте
+    }
+
+    static void loadResource(File pFile) {
+        FileReader reader = new FileReader(pFile)
+        RestTemplate restTemplate = new RestTemplate()
+        restTemplate.postForLocation("http://localhost:8080/engine-rest/deployment/create", reader)
     }
 }
