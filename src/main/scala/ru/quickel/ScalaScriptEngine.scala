241789443bb9a87ac9c3a933cf0cde6ee41effd9
areful <areful@yandex.ru>
2022-06-09 00:07:13 +0300

    202206090006

--- src/main/scala/ru/quickel/ScalaScriptEngine.scala
+++ src/main/scala/ru/quickel/ScalaScriptEngine.scala
@@ -1,5 +1,8 @@
 package ru.quickel
 
+import javax.script.{ScriptEngine, ScriptEngineManager}
+
 object ScalaScriptEngine {
+  val engine: ScriptEngine = new ScriptEngineManager().getEngineByName("scala")
 
 }
